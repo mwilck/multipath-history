1d7e9e941f748c7d33e0435b46970e7d721bd5cd
Christophe Varoqui <christophe.varoqui@free.fr>
2005-03-19 15:11:00 +0100

    libmultipath: add dm_get_opencount

--- libmultipath/devmapper.c
+++ libmultipath/devmapper.c
@@ -235,6 +235,31 @@ dm_type(char * name, char * type)
 	return r;
 }
 
+int
+dm_get_opencount (char * mapname)
+{
+	int r = -1;
+	struct dm_task *dmt;
+	struct dm_info info;
+
+	if (!(dmt = dm_task_create(DM_DEVICE_INFO)))
+		return 0;
+
+	if (!dm_task_set_name(dmt, mapname))
+		goto out;
+
+	if (!dm_task_run(dmt))
+		goto out;
+
+	if (!dm_task_get_info(dmt, &info))
+		goto out;
+
+	r = info.open_count;
+out:
+	dm_task_destroy(dmt);
+	return r;
+}
+	
 extern int
 dm_flush_maps (char * type)
 {
@@ -257,6 +282,7 @@ dm_flush_maps (char * type)
 
 	do {
 		if (dm_type(names->name, type) &&
+		    dm_get_opencount(names->name) == 0 &&
 		    !dm_simplecmd(DM_DEVICE_REMOVE, names->name))
 			r++;
 
