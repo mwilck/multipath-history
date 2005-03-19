715a16d6585246f850c6bc2cd9e6cd7c02a2d869
Christophe Varoqui <christophe.varoqui@free.fr>
2005-03-19 15:11:00 +0100

    libmultipath: optimize devnode blacklist

--- libmultipath/blacklist.c
+++ libmultipath/blacklist.c
@@ -12,21 +12,24 @@ setup_default_blist (vector blist)
 {
 	char * str;
 
-	VECTOR_ADDSTR(blist, "c0d");
-	VECTOR_ADDSTR(blist, "c1d");
-	VECTOR_ADDSTR(blist, "c2d");
-	VECTOR_ADDSTR(blist, "fd");
-	VECTOR_ADDSTR(blist, "hd");
-	VECTOR_ADDSTR(blist, "md");
-	VECTOR_ADDSTR(blist, "dm");
-	VECTOR_ADDSTR(blist, "sr");
-	VECTOR_ADDSTR(blist, "scd");
-	VECTOR_ADDSTR(blist, "st");
 	VECTOR_ADDSTR(blist, "ram");
 	VECTOR_ADDSTR(blist, "raw");
 	VECTOR_ADDSTR(blist, "loop");
+	VECTOR_ADDSTR(blist, "fd");
+	VECTOR_ADDSTR(blist, "hda");
+	VECTOR_ADDSTR(blist, "hdb");
+	VECTOR_ADDSTR(blist, "hdc");
+	VECTOR_ADDSTR(blist, "hdd");
+	VECTOR_ADDSTR(blist, "md");
+	VECTOR_ADDSTR(blist, "dm-");
+	VECTOR_ADDSTR(blist, "sr");
+	VECTOR_ADDSTR(blist, "scd");
+	VECTOR_ADDSTR(blist, "st");
 	VECTOR_ADDSTR(blist, "nbd");
 	VECTOR_ADDSTR(blist, "ub");
+	VECTOR_ADDSTR(blist, "c0d");
+	VECTOR_ADDSTR(blist, "c1d");
+	VECTOR_ADDSTR(blist, "c2d");
 }
 
 int
