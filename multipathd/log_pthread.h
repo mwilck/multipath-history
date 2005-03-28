f519203808767a1bf28b2ddb5ad60a78cb983837
Christophe Varoqui <christophe.varoqui@free.fr>
2005-03-29 00:36:00 +0200

    [multipathd] asynchronous & non-blocking logger thread
    Implementation split into a generic log
    lib and a pthread wrapper for locking and events.
    An ipc wrapper could be easily created by
    interested parties.
    
    Replace syslog() calls with log_safe().

---
+++ multipathd/log_pthread.h
@@ -0,0 +1,7 @@
+pthread_mutex_t *logq_lock;
+pthread_mutex_t *logev_lock;
+pthread_cond_t *logev_cond;
+
+void log_safe(int prio, char * fmt, ...);
+void log_thread_start(void);
+void log_thread_stop(void);
