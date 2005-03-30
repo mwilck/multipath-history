c75a0a9408b32b2b64642f7a7a4c0c5786ef05f3
Christophe Varoqui <christophe.varoqui@free.fr>
2005-03-31 01:28:00 +0200

    libmultipath: remove find_hwe()

--- libmultipath/hwtable.c
+++ libmultipath/hwtable.c
@@ -5,26 +5,11 @@
 #include "vector.h"
 #include "structs.h"
 #include "defaults.h"
+#include "config.h"
 #include "hwtable.h"
-#include "util.h"
+#include "pgpolicies.h"
 
 #include "../libcheckers/checkers.h"
-#include "../multipath/pgpolicies.h"
-
-extern struct hwentry *
-find_hwe (vector hwtable, char * vendor, char * product)
-{
-	int i;
-	struct hwentry * hwe;
-
-	vector_foreach_slot (hwtable, hwe, i) {
-		if (strcmp_chomp(hwe->vendor, vendor) == 0 &&
-		    (hwe->product[0] == '*' ||
-		    strcmp_chomp(hwe->product, product) == 0))
-			return hwe;
-	}
-	return NULL;
-}
 
 void
 setup_default_hwtable (vector hw)
