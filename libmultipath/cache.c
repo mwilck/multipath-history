881ba74d83b330bf97f9f8d0d1669e94e36b2319
Christophe Varoqui <christophe.varoqui@free.fr>
2005-04-11 16:56:00 +0200

    cache: add locking for cache file
    Add the lock_fd() helper. Remove cache_flush().
    Move cache file location from /dev/shm to /dev.

--- libmultipath/cache.c
+++ libmultipath/cache.c
@@ -8,13 +8,40 @@
 
 #include "vector.h"
 #include "structs.h"
+#include "debug.h"
 #include "cache.h"
+
+static void
+revoke_cache_info(struct path * pp)
+{
+	pp->fd = 0;
+}
+
+static int
+lock_fd (int fd, int flag)
+{
+	struct flock fl;
+
+	fl.l_type = flag;
+	fl.l_whence = 0;
+	fl.l_start = 0;
+	fl.l_len = 0;
+
+	alarm(MAX_WAIT);
+
+	if (fcntl(fd, F_SETLKW, &fl) == -1) {
+		condlog(0, "can't take a write lease on cache file\n");
+		return 1;
+	}
+	alarm(0);
+	return 0;
+}
 
 int
 cache_load (vector pathvec)
 {
 	int fd;
-	struct flock fl;
+	int r = 1;
 	off_t record_len;
 	struct path record;
 	struct path * pp;
@@ -24,31 +51,30 @@ cache_load (vector pathvec)
 	if (fd < 0)
 		return 1;
 
-	fl.l_type = F_RDLCK;
-	fl.l_whence = 0;
-	fl.l_start = 0;
-	fl.l_len = 0;
-
-	alarm(MAX_WAIT);
-
-	if (fcntl(fd, F_SETLKW, &fl) == -1) {
-		fprintf(stderr, "can't take a read lease on %s\n", CACHE_FILE);
-		close(fd);
-		return 1;
-	}
-	alarm(0);
+	if (lock_fd(fd, F_RDLCK))
+		goto out;
 
 	record_len = sizeof(struct path);
 
 	while (read(fd, &record, record_len)) {
 		pp = alloc_path();
-		vector_alloc_slot(pathvec);
+
+		if (!pp)
+			goto out;
+
+		if (!vector_alloc_slot(pathvec)) {
+			free_path(pp);
+			goto out;
+		}
 		vector_set_slot(pathvec, pp);
 		memcpy(pp, &record, record_len);
-		pp->fd = 0;
+		revoke_cache_info(pp);
 	}
+	r = 0;
+	lock_fd(fd, F_UNLCK);
+out:
 	close(fd);
-	return 0;
+	return r;
 }
 
 int
@@ -56,67 +82,32 @@ cache_dump (vector pathvec)
 {
 	int i;
 	int fd;
-	struct flock fl;
+	int r = 1;
 	off_t record_len;
 	struct path * pp;
 
-	fd = open(CACHE_FILE, O_RDWR|O_CREAT);
+	fd = open(CACHE_TMPFILE, O_RDWR|O_CREAT);
 
 	if (fd < 0)
 		return 1;
 
-	fl.l_type = F_WRLCK;
-	fl.l_whence = 0;
-	fl.l_start = 0;
-	fl.l_len = 0;
+	if (lock_fd(fd, F_WRLCK))
+		goto out;
 
-	alarm(MAX_WAIT);
-
-	if (fcntl(fd, F_SETLKW, &fl) == -1) {
-		fprintf(stderr, "can't take a write lease on %s\n", CACHE_FILE);
-		close(fd);
-		return 1;
-	}
-	alarm(0);
 	ftruncate(fd, 0); 
 	record_len = sizeof(struct path);
 
 	vector_foreach_slot (pathvec, pp, i) {
-		if (write(fd, pp, record_len) < record_len) {
-			close(fd);
-			return 1;
-		}
+		if (write(fd, pp, record_len) < record_len)
+			goto out1;
 	}
+	rename(CACHE_TMPFILE, CACHE_FILE);
+	r = 0;
+out1:
+	lock_fd(fd, F_UNLCK);
+out:
 	close(fd);
-	return 0;
-}
-
-int
-cache_flush (void)
-{
-	int fd;
-	struct flock fl;
-
-	fd = open(CACHE_FILE, O_RDWR|O_CREAT);
-
-	if (fd < 0)
-		return 1;
-
-	fl.l_type = F_WRLCK;
-	fl.l_whence = 0;
-	fl.l_start = 0;
-	fl.l_len = 0;
-
-	alarm(MAX_WAIT);
-
-	if (fcntl(fd, F_SETLKW, &fl) == -1) {
-		fprintf(stderr, "can't take a write lease on %s\n", CACHE_FILE);
-		close(fd);
-		return 1;
-	}
-	alarm(0);
-
-	return ftruncate(fd, 0);
+	return r;
 }
 
 int
--- libmultipath/cache.h
+++ libmultipath/cache.h
@@ -1,8 +1,8 @@
-#define CACHE_FILE	"/dev/shm/multipath.cache"
+#define CACHE_FILE	"/dev/.multipath.cache"
+#define CACHE_TMPFILE	"/dev/.multipath.cache.swp"
 #define CACHE_EXPIRE	5
 #define MAX_WAIT	5
 
 int cache_load (vector pathvec);
 int cache_dump (vector pathvec);
 int cache_cold (int expire);
-int cache_flush (void);
