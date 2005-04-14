89ecd361b9a4657b80dff448ee9ddd2e55e579e3
Christophe Varoqui <christophe.varoqui@free.fr>
2005-04-15 00:00:00 +0200

    libmultipath: set default udev dir to /dev

--- libmultipath/defaults.h
+++ libmultipath/defaults.h
@@ -1,5 +1,5 @@
 #define DEFAULT_GETUID		"/sbin/scsi_id -g -u -s /block/%n"
-#define DEFAULT_UDEVDIR		"/udev"
+#define DEFAULT_UDEVDIR		"/dev"
 #define DEFAULT_SELECTOR	"round-robin 0"
 #define DEFAULT_FEATURES	"0"
 #define DEFAULT_HWHANDLER	"0"
