openSUSE/multipath-tools-pre2021

Use 'mptable' as argument for find_mpe() and get_mpe_wwid()
Signed-off-by: Hannes Reinecke <hare@suse.com>

libmultipath/config.h
@@ -179,8 +179,8 @@ struct config {
 struct config * conf;
 
 struct hwentry * find_hwe (vector hwtable, char * vendor, char * product, char *revision);
-struct mpentry * find_mpe (char * wwid);
-char * get_mpe_wwid (char * alias);
+struct mpentry * find_mpe (vector hwtable, char * wwid);
+char * get_mpe_wwid (vector hwtable, char * alias);
 
 struct hwentry * alloc_hwe (void);
 struct mpentry * alloc_mpe (void);

libmultipath/configure.c
@@ -1042,7 +1042,7 @@ get_refwwid (char * dev, enum devtypes dev_type, vector pathvec, char **wwid)
 		/*
 		 * or may be an alias
 		 */
-		refwwid = get_mpe_wwid(dev);
+		refwwid = get_mpe_wwid(conf->hwtable, dev);
 
 		/*
 		 * or directly a wwid

libmultipath/propsel.c
@@ -409,7 +409,7 @@ select_prio (struct path * pp)
 			goto out;
 		}
 	}
-	mpe = find_mpe(pp->wwid);
+	mpe = find_mpe(conf->hwtable, pp->wwid);
 	set_prio(mpe, "(LUN setting)");
 	set_prio(conf->overrides, "(overrides setting)");
 	set_prio(pp->hwe, "controller setting)");

libmultipath/structs_vec.c
@@ -385,7 +385,7 @@ __setup_multipath (struct vectors * vecs, struct multipath * mpp, int reset)
 	}
 
 	set_multipath_wwid(mpp);
-	mpp->mpe = find_mpe(mpp->wwid);
+	mpp->mpe = find_mpe(conf->hwtable, mpp->wwid);
 	condlog(3, "%s: discover", mpp->alias);
 
 	if (!mpp->hwe)
@@ -469,7 +469,7 @@ add_map_with_path (struct vectors * vecs,
 	if (!(mpp = alloc_multipath()))
 		return NULL;
 
-	mpp->mpe = find_mpe(pp->wwid);
+	mpp->mpe = find_mpe(conf->hwtable, pp->wwid);
 	mpp->hwe = pp->hwe;
 
 	strcpy(mpp->wwid, pp->wwid);

mwilck/multipath-history

(cherry picked from commit 8cc0f7921aba99ef034541fe6658c07228b90d81)

libmultipath/config.h
@@ -179,8 +179,8 @@ struct config {
 struct config * conf;
 
 struct hwentry * find_hwe (vector hwtable, char * vendor, char * product, char *revision);
-struct mpentry * find_mpe (char * wwid);
-char * get_mpe_wwid (char * alias);
+struct mpentry * find_mpe (vector mptable, char * wwid);
+char * get_mpe_wwid (vector mptable, char * alias);
 
 struct hwentry * alloc_hwe (void);
 struct mpentry * alloc_mpe (void);

libmultipath/configure.c
@@ -1042,7 +1042,7 @@ get_refwwid (char * dev, enum devtypes dev_type, vector pathvec, char **wwid)
 		/*
 		 * or may be an alias
 		 */
-		refwwid = get_mpe_wwid(dev);
+		refwwid = get_mpe_wwid(conf->mptable, dev);
 
 		/*
 		 * or directly a wwid

libmultipath/propsel.c
@@ -409,7 +409,7 @@ select_prio (struct path * pp)
 			goto out;
 		}
 	}
-	mpe = find_mpe(pp->wwid);
+	mpe = find_mpe(conf->mptable, pp->wwid);
 	set_prio(mpe, "(LUN setting)");
 	set_prio(conf->overrides, "(overrides setting)");
 	set_prio(pp->hwe, "controller setting)");

libmultipath/structs_vec.c
@@ -385,7 +385,7 @@ __setup_multipath (struct vectors * vecs, struct multipath * mpp, int reset)
 	}
 
 	set_multipath_wwid(mpp);
-	mpp->mpe = find_mpe(mpp->wwid);
+	mpp->mpe = find_mpe(conf->mptable, mpp->wwid);
 	condlog(3, "%s: discover", mpp->alias);
 
 	if (!mpp->hwe)
@@ -469,7 +469,7 @@ add_map_with_path (struct vectors * vecs,
 	if (!(mpp = alloc_multipath()))
 		return NULL;
 
-	mpp->mpe = find_mpe(pp->wwid);
+	mpp->mpe = find_mpe(conf->mptable, pp->wwid);
 	mpp->hwe = pp->hwe;
 
 	strcpy(mpp->wwid, pp->wwid);